03ee78c7647169bbb6cf0bd93faf268bae323133
brad11dawson <brad11dawson@gmail.com>
2019-09-21 07:18:04 -0600

    Implemented recycler view for parties on home pagae, and changes dependin on which school is selected

--- app/src/main/java/com/partytime/myparty/MainActivity.kt
+++ app/src/main/java/com/partytime/myparty/MainActivity.kt
@@ -1,16 +1,93 @@
 package com.partytime.myparty
 
+import android.content.ClipDescription
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.Spinner
+import android.widget.TextView
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    // [START declare_database]
+    private lateinit var db: FirebaseFirestore
+    // [END declare_database]
+    private var partyList = ArrayList<PartyListItem>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        db = FirebaseFirestore.getInstance()
+
+        val schoolSpinner: Spinner = findViewById(R.id.schoolSpinner)
+        schoolSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+
+            }
+
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                val school: String = schoolSpinner.selectedItem.toString()
+                partyList.clear()
+                fillPartyList(school)
+            }
+        }
+
+        partyListView.hasFixedSize()
+        partyListView.setLayoutManager(LinearLayoutManager(this))
+        partyListView.itemAnimator = DefaultItemAnimator()
+        partyListView.adapter = RecyclerAdapter(partyList, R.layout.partyrowlayout)
+    }
+
+    class RecyclerAdapter(val parties : List<PartyListItem>, val itemLayout: Int) : RecyclerView.Adapter<RecyclerViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewHolder {
+            var view = LayoutInflater.from(parent.context).inflate(itemLayout, parent, false)
+            return RecyclerViewHolder(view)
+        }
+
+        override fun getItemCount(): Int {
+            return parties.size
+        }
+
+        override fun onBindViewHolder(holder: RecyclerViewHolder, position: Int) {
+            var party = parties.get(position)
+            holder.updateParty(party)
+        }
+
+    }
+
+    class RecyclerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private var nameView: TextView
+        private var descriptionView: TextView
+
+        init {
+            nameView = itemView.findViewById<TextView>(R.id.partyNameView)
+            descriptionView = itemView.findViewById<TextView>(R.id.partyDescription)
+        }
+
+        fun updateParty(party: PartyListItem) {
+            nameView?.text = party.partyName
+            descriptionView?.text = party.partyDescription
+        }
+    }
+
+    class PartyListItem(name: String, description: String) {
+        var partyName: String
+        var partyDescription: String
+
+        init {
+            partyName = name
+            partyDescription = description
+        }
     }
 
     fun signOut(view: View) {
@@ -22,5 +99,27 @@ class MainActivity : AppCompatActivity() {
     fun gotoAddParty(view: View) {
         val intent = Intent(this, addPartyActivity::class.java)
         startActivity(intent)
+    }
+
+
+    fun fillPartyList(school: String) {
+        db.collection("parties")
+            .whereEqualTo("school", school)
+            .get()
+            .addOnSuccessListener { documents ->
+                for (document in documents) {
+                    val name = document.data.getValue("title").toString()
+                    val description = document.data.getValue("description").toString()
+                    Log.d(TAG, "title of document: $name")
+                    Log.d(TAG, "description of document: $description")
+                    val partylistItem = PartyListItem(name, description)
+                    partyList.add(partylistItem)
+                }
+                partyListView.adapter?.notifyDataSetChanged()
+            }
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
     }
 }
