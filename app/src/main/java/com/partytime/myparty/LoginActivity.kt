8877eaad289a3bb2a2f66a6e1fadee44d8c10cad
brad11dawson <brad11dawson@gmail.com>
2019-09-20 20:10:02 -0600

    added layouts for login and signup pages

---
+++ app/src/main/java/com/partytime/myparty/LoginActivity.kt
@@ -0,0 +1,20 @@
+package com.partytime.myparty
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    fun gotoSignup(view: View) {
+        val intent = Intent(this, SignupActivity::class.java)
+        startActivity(intent)
+    }
+}
