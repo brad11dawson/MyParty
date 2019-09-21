046b89d170fe21a253f841e8bfde5d0f0699353c
brad11dawson <brad11dawson@gmail.com>
2019-09-21 00:19:09 -0600

    Added age to signup

--- app/src/main/java/com/partytime/myparty/SignupActivity.kt
+++ app/src/main/java/com/partytime/myparty/SignupActivity.kt
@@ -3,6 +3,7 @@ package com.partytime.myparty
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.util.Log
 import android.view.View
 import android.widget.EditText
@@ -35,9 +36,12 @@ class SignupActivity : AppCompatActivity() {
     }
     // [END on_start_check_user]
 
-    private fun createAccount(email: String, password: String) {
-        Log.d(TAG, "createAccount:$email")
+    private fun createAccount(email: String, password: String, fName: String, lName: String, age: Int) {
+        Log.d(TAG, "createEmail:$email")
         Log.d(TAG, "createPassword:$password")
+        Log.d(TAG, "createFirstname:$fName")
+        Log.d(TAG, "createLastName:$lName")
+        Log.d(TAG, "createAge:$age")
 
         auth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
@@ -46,19 +50,78 @@ class SignupActivity : AppCompatActivity() {
                     gotoMain()
                 } else {
                     Log.w(TAG, "createUserWithEmail:failure", task.exception)
-                    Toast.makeText(baseContext, "Authentication failed.",
-                        Toast.LENGTH_SHORT).show()
+                    Toast.makeText(baseContext, "Please enter a valid email and password. Passwords must " +
+                            "contain at least 6 characters.",
+                        Toast.LENGTH_LONG).show()
                 }
             }
         // [END create_user_with_email]
     }
 
+    private fun validateSignup(emailInput: EditText, passwordInput: EditText, fNameInput: EditText, lNameInput: EditText, ageInput: EditText): Boolean {
+        var valid = true
+
+        val fName = fNameInput.text.toString()
+        if (TextUtils.isEmpty(fName)) {
+            fNameInput.error = "Required."
+            valid = false
+        } else {
+            fNameInput.error = null
+        }
+
+        val lName = lNameInput.text.toString()
+        if (TextUtils.isEmpty(lName)) {
+            lNameInput.error = "Required."
+            valid = false
+        } else {
+            lNameInput.error = null
+        }
+
+        val email = emailInput.text.toString()
+        if (TextUtils.isEmpty(email)) {
+            emailInput.error = "Required."
+            valid = false
+        } else {
+            emailInput.error = null
+        }
+
+        val age = ageInput.text.toString()
+        if (TextUtils.isEmpty(age)) {
+            ageInput.error = "Required."
+            valid = false
+        } else {
+            ageInput.error = null
+        }
+
+        val password = passwordInput.text.toString()
+        if (TextUtils.isEmpty(password)) {
+            passwordInput.error = "Required."
+            valid = false
+        } else {
+            passwordInput.error = null
+        }
+
+        return valid
+    }
+
     fun clickSignup(view: View) {
+        val firstNameText = findViewById<EditText>(R.id.firstNameInput)
+        val lastNameText = findViewById<EditText>(R.id.lastNameInput)
+        val ageText = findViewById<EditText>(R.id.ageInput)
         val emailText = findViewById<EditText>(R.id.emailInput)
-        val email = emailText.text.toString()
         val passwordText = findViewById<EditText>(R.id.passwordInput)
-        val password = passwordText.text.toString()
-        createAccount(email, password)
+
+        val valid = validateSignup(emailText, passwordText, firstNameText, lastNameText, ageText)
+
+        if (valid) {
+            val fName = firstNameText.text.toString()
+            val lName = lastNameText.text.toString()
+            val ageString = ageText.text.toString()
+            val age = Integer.parseInt(ageString)
+            val email = emailText.text.toString()
+            val password = passwordText.text.toString()
+            createAccount(email, password, fName, lName, age)
+        }
     }
 
     fun gotoLogin(view: View) {
