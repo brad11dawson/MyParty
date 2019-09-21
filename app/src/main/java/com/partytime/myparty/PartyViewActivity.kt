fea15377204bcfa50537bd711537c9171a15bd7f
desertalex <alexbraddock22@gmail.com>
2019-09-21 07:40:49 -0600

    Saved from hell

--- app/src/main/java/com/partytime/myparty/PartyViewActivity.kt
+++ app/src/main/java/com/partytime/myparty/PartyViewActivity.kt
@@ -5,8 +5,13 @@ import android.os.Bundle
 
 class PartyViewActivity : AppCompatActivity() {
 
+    val partyID : String = "7hDMmBwilRCZ6McKesC1"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_party_view)
     }
+
+
+
 }
