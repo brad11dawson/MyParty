54df5086b2065de27e66348652d75ee9d3fb75ee
brad11dawson <brad11dawson@gmail.com>
2019-09-20 21:40:28 -0600

    Merge branch 'master' of C:\Users\brad1\MyParty with conflicts.

--- app/src/main/java/com/partytime/myparty/SignupActivity.kt
+++ app/src/main/java/com/partytime/myparty/SignupActivity.kt
@@ -3,17 +3,66 @@ package com.partytime.myparty
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class SignupActivity : AppCompatActivity() {
+    // [START declare_auth]
+    private lateinit var auth: FirebaseAuth
+    // [END declare_auth]
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        // [START initialize_auth]
+        // Initialize Firebase Auth
+        auth = FirebaseAuth.getInstance()
+        // [END initialize_auth]
+    }
+
+    // [START on_start_check_user]
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        //val currentUser = auth.currentUser
+    }
+    // [END on_start_check_user]
+
+    private fun createAccount(email: String, password: String) {
+        Log.d(TAG, "createAccount:$email")
+        Log.d(TAG, "createPassword:$password")
+
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "createUserWithEmail:success")
+                } else {
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+        // [END create_user_with_email]
+    }
+
+    fun clickSignup(view: View) {
+        val emailText = findViewById<EditText>(R.id.emailInput)
+        val email = emailText.text.toString()
+        val passwordText = findViewById<EditText>(R.id.passwordInput)
+        val password = passwordText.text.toString()
+        createAccount(email, password)
     }
 
     fun gotoLogin(view: View) {
         val intent = Intent(this, LoginActivity::class.java)
         startActivity(intent)
+    }
+
+    companion object {
+        private const val TAG = "SignupActivity"
     }
 }
